80d2834764c27b76d7b70f6f95e13cd3ae67aad5
VaitkusM <s.high.hopes@gmail.com>
2025-04-14 16:08:43 +0200

    Fixed libgeom include

--- include/libcdgbs/Example.hpp
+++ include/libcdgbs/Example.hpp
@@ -2,10 +2,11 @@
 #include <Eigen/Dense>
 #include <triangle.h>
 #include <triangle_api.h>
+#include <geometry.hh>
 
 namespace libcdgbs {
     Eigen::MatrixXd m;
     context* triangle_ctx;
-
+    Geometry::BSSurface rib;
     void say_hello();
 }
