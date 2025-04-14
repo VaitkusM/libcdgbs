cfe8f763462d2af2e18677e0d2be4572c653a0ca
VaitkusM <s.high.hopes@gmail.com>
2025-04-14 13:44:38 +0200

    Initial commit

---
+++ src/Example.cpp
@@ -0,0 +1,8 @@
+#include "libcdgbs/Example.hpp"
+#include <iostream>
+
+namespace libcdgbs {
+    void say_hello() {
+        std::cout << "Hello from libcdgbs!" << std::endl;
+    }
+}
---
+++ tests/main_test.cpp
@@ -0,0 +1,6 @@
+#include "libcdgbs/Example.hpp"
+
+int main() {
+    libcdgbs::say_hello();
+    return 0;
+}
